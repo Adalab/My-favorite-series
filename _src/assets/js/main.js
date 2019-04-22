45e8c7421764758b2c366b72281f283156397431
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-22 09:21:38 +0200

    search functioning in console

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('>> Ready :)');
+const searchInput = document.querySelector ('.form__input');
+const searchButton = document.querySelector ('.button__input');
+
+const urlFetch = 'http://api.tvmaze.com/search/shows?q=';
+
+function searchSeries () {
+  const inputContain = searchInput.value;
+  fetch (`${urlFetch}${inputContain}`)
+    .then (response => response.json ())
+    .then (data => {
+      //   const serieTitle = data[0].toLowerCase ();
+      //   const inputLowerCase = inputContain.toLowerCase ();
+      for (const series of data) {
+        const titleSeries = series.show.name;
+
+        const titleSeriesLower = titleSeries.toLowerCase ();
+        const inputContainLower = inputContain.toLowerCase ();
+
+        if (titleSeriesLower.includes (inputContainLower)) {
+          console.log (titleSeries);
+        }
+      }
+    });
+}
+
+searchButton.addEventListener ('click', searchSeries);
