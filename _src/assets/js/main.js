240ca87401a120ff5e77616e74be8ed227e6b644
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-04-22 09:24:55 +0200

    make a function to made the comparison

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,22 +5,24 @@ const searchButton = document.querySelector ('.button__input');
 
 const urlFetch = 'http://api.tvmaze.com/search/shows?q=';
 
+const searchFunction = (data, inputContain) => {
+  const titleSeriesLower = data.toLowerCase ();
+  const inputContainLower = inputContain.toLowerCase ();
+
+  if (titleSeriesLower.includes (inputContainLower)) {
+    console.log (data);
+  }
+};
+
 function searchSeries () {
   const inputContain = searchInput.value;
+
   fetch (`${urlFetch}${inputContain}`)
     .then (response => response.json ())
     .then (data => {
-      //   const serieTitle = data[0].toLowerCase ();
-      //   const inputLowerCase = inputContain.toLowerCase ();
       for (const series of data) {
         const titleSeries = series.show.name;
-
-        const titleSeriesLower = titleSeries.toLowerCase ();
-        const inputContainLower = inputContain.toLowerCase ();
-
-        if (titleSeriesLower.includes (inputContainLower)) {
-          console.log (titleSeries);
-        }
+        searchFunction (titleSeries, inputContain);
       }
     });
 }
